8b5c9de9d3ace938ef2b1b93c5b4e9b111368305
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-09-25 12:30:48 +0300

    Create mpesa.php

---
+++ config/mpesa.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'environment' => env('MPESA_ENVIRONMENT', 'sandbox'),
+    'consumer_key' => env('MPESA_CONSUMER_KEY'),
+    'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
+    // Add shortcode, passkey, etc.
+];
